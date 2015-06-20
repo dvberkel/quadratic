454a4f68664d10f001ab47caad8d3be344f5b306
dvberkel <daan.v.berkel.1980@gmail.com>
2015-06-20 07:21:48 +0200

    Show solution steps on solve click

--- js/application.js
+++ js/application.js
@@ -11,5 +11,13 @@
     new quadratic.EquationView(equation, document.getElementById('equation'));
 
     var solution = quadratic.solve(equation);
-    new quadratic.SolutionView(solution, document.getElementById('solution-steps'));
+    var solutionSteps = document.getElementById('solution-steps');
+    new quadratic.SolutionView(solution, solutionSteps);
+
+    function showSolutionSteps(){
+        solutionSteps.classList.add('show');
+    }
+
+    var solve = document.getElementById('solve');
+    solve.addEventListener('click', showSolutionSteps);
 })(quadratic);
